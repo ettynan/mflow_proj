104b622cb1da197e5c14c0e63850c598b8be0afb
ettynan <ettynan@gmail.com>
2023-07-10 12:36:40 -0700

    MLflow example with wine quality

--- train.py
+++ train.py
@@ -2,7 +2,6 @@
 # P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
 # Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
 
-import os
 import warnings
 import sys
 
