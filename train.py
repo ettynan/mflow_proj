82dc92ef90369039751b3ba9f043e70e53717c05
ettynan <ettynan@gmail.com>
2023-07-10 12:55:15 -0700

    Formatting error corrections

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+# The data set used in this example is from
+# http://archive.ics.uci.edu/ml/datasets/Wine+Quality
 # P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+# Modeling wine preferences by data mining from physicochemical properties.
+# In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
 
 import warnings
 import sys
-
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -40,7 +41,8 @@ if __name__ == "__main__":
         data = pd.read_csv(csv_url, sep=";")
     except Exception as e:
         logger.exception(
-            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+            "Unable to download training & test CSV, check your internet \
+                connection. Error: %s", e
         )
 
     # Split the data into training and test sets. (0.75, 0.25) split.
@@ -63,7 +65,8 @@ if __name__ == "__main__":
 
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(
+            alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
@@ -82,11 +85,13 @@ if __name__ == "__main__":
         # Model registry does not work with file store
         if tracking_url_type_store != "file":
             # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
+            # There are other ways to use the Model Registry, which depends on
+            # the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel", signature=signature
+                lr, "model", registered_model_name="ElasticnetWineModel",
+                signature=signature
             )
         else:
             mlflow.sklearn.log_model(lr, "model", signature=signature)
